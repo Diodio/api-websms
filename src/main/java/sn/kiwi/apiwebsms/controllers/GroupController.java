585b10ad28460e8e2335ce528b6a66fc8a37f0c0
matgere <matar.gueye@kiwi.sn>
2023-07-28 14:27:44 +0000

    my initial commit

--- src/main/java/sn/kiwi/apiwebsms/controllers/GroupController.java
+++ src/main/java/sn/kiwi/apiwebsms/controllers/GroupController.java
@@ -1,11 +1,11 @@
 package sn.kiwi.apiwebsms.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +18,15 @@ import org.springframework.web.client.RestTemplate;
 import sn.kiwi.apiwebsms.common.Common;
 import sn.kiwi.apiwebsms.config.PathsProperties;
 import sn.kiwi.apiwebsms.dtos.ApiDtoResponse;
-import sn.kiwi.apiwebsms.dtos.ContactsGroupDto;
 import sn.kiwi.apiwebsms.dtos.GroupListDto;
+import sn.kiwi.apiwebsms.dtos.GroupListWithContactsDto;
 import sn.kiwi.apiwebsms.dtos.MessageDetailDto;
 import sn.kiwi.apiwebsms.models.GroupAddModel;
 import sn.kiwi.apiwebsms.models.GroupDeleteModel;
 import sn.kiwi.apiwebsms.models.GroupListModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class GroupController {
@@ -53,7 +56,6 @@ public class GroupController {
         logger.trace("file: GroupController.java, getAllGroups, userId:"+groupListModel.getUser_id()+", login: "+groupListModel.getLogin()+"," +
                 "costumerId: "+groupListModel.getCustomer_id()+" clientId: orangesn, urlBackend: /group/GroupController.php, ACTION: LIST");
         ObjectMapper mapper = new ObjectMapper();
-        try {
         String backendUrl = pathsProperties.getPathValue("backend.url") + "/group/GroupController.php";
         RestTemplate restTemplate=new RestTemplate();
         HttpHeaders requestHeaders = common.setUserCookies(pathsProperties, groupListModel.getLogin(), groupListModel.getPassword(), groupListModel.getPartnerId());
@@ -78,10 +80,7 @@ public class GroupController {
             logger.trace("************************** End to get all groups ************************************");
             return ResponseEntity.ok(groups);
         }
-    } catch (Exception e) {
-        logger.trace(e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error while processing your request. Please contact your administrator.");
-    }
+
     }
 
     @PostMapping(value = "groups")
@@ -99,7 +98,6 @@ public class GroupController {
         logger.trace("file: GroupController.java, createGroup, userId:"+groupAddModel.getUser_id()+", login: "+groupAddModel.getLogin()+"," +
                 "costumerId: "+groupAddModel.getPartnerId()+" clientId: orangesn, urlBackend: /group/GroupController.php, ACTION: INSERT");
         ObjectMapper mapper = new ObjectMapper();
-        try {
         String backendUrl = pathsProperties.getPathValue("backend.url") + "/group/GroupController.php";
         RestTemplate restTemplate=new RestTemplate();
         HttpHeaders requestHeaders = common.setUserCookies(pathsProperties, groupAddModel.getLogin(), groupAddModel.getPassword(), groupAddModel.getPartnerId());
@@ -118,10 +116,6 @@ public class GroupController {
         }
             logger.trace("New group created: Id: "+groupAddModel.getUser_id()+" groupName: "+groupAddModel.getName());
             return ResponseEntity.ok(new ApiDtoResponse(true, "A new group is successfully created."));
-    } catch (Exception e) {
-        logger.trace(e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error while processing your request. Please contact your administrator.");
-    }
     }
 
     @PutMapping(value = "groups")
@@ -139,7 +133,6 @@ public class GroupController {
         logger.trace("file: GroupController.java, updateGroup, userId:"+groupAddModel.getUser_id()+", login: "+groupAddModel.getLogin()+"," +
                 "costumerId: "+groupAddModel.getPartnerId()+" clientId: orangesn, urlBackend: /group/GroupController.php, ACTION: UPDATE");
         ObjectMapper mapper = new ObjectMapper();
-        try {
         String backendUrl = pathsProperties.getPathValue("backend.url") + "/group/GroupController.php";
         RestTemplate restTemplate=new RestTemplate();
         HttpHeaders requestHeaders = common.setUserCookies(pathsProperties, groupAddModel.getLogin(), groupAddModel.getPassword(), groupAddModel.getPartnerId());
@@ -160,10 +153,6 @@ public class GroupController {
             logger.trace("Group, groupeId: "+groupAddModel.getId()+" was successfully updated");
             logger.trace("************************** Start to update group ************************************");
         return ResponseEntity.ok(new ApiDtoResponse(true, "Group is successfully updated."));
-    } catch (Exception e) {
-        logger.trace(e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error while processing your request. Please contact your administrator.");
-    }
 
     }
 
@@ -182,7 +171,6 @@ public class GroupController {
         logger.trace("file: GroupController.java, deleteGroup, userId:"+groupDeleteModel.getUser_id()+", login: "+groupDeleteModel.getLogin()+"," +
                 "costumerId: "+groupDeleteModel.getPartnerId()+" clientId: orangesn, urlBackend: /group/GroupController.php, ACTION: UPDATE");
         ObjectMapper mapper = new ObjectMapper();
-        try {
         String backendUrl = pathsProperties.getPathValue("backend.url") + "/group/GroupController.php";
         RestTemplate restTemplate=new RestTemplate();
         HttpHeaders requestHeaders = common.setUserCookies(pathsProperties, groupDeleteModel.getLogin(), groupDeleteModel.getPassword(), groupDeleteModel.getPartnerId());
@@ -200,65 +188,7 @@ public class GroupController {
         }
         logger.trace("Group, groupId: "+groupDeleteModel.getGroup_id()+" was successfully deleted");
         logger.trace("************************** Start to delete group ************************************");
-        return ResponseEntity.ok(new ApiDtoResponse(true, "Group is successfully deleted."));
-    } catch (Exception e) {
-        logger.trace(e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error while processing your request. Please contact your administrator.");
-    }
-    }
-
-
-    @GetMapping(value = "groups/contacts")
-    @Operation(
-            tags = {"Groups"},
-            operationId = "Groups",
-            summary = "List contacts by group",
-            description = "Show the list contacts by group",
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "List contacts by group.",
-                    content = @Content(schema = @Schema(implementation = GroupListModel.class))),
-            responses = {@ApiResponse(responseCode = "200", description = "Found the list contacts by groups",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = GroupListDto.class))})}
-    )
-
-    public ResponseEntity<?> getContactsByGroup(@RequestBody GroupListModel groupListModel) throws Exception {
-        logger.trace("************************** Start to get contacts by group ************************************");
-        logger.trace("file: GroupController.java, getContactsByGroup, userId:"+groupListModel.getUser_id()+", login: "+groupListModel.getLogin()+"," +
-                "costumerId: "+groupListModel.getCustomer_id()+" clientId: orangesn, urlBackend: /group/GroupController.php, ACTION: LIST_CONTACTS_BY_GROUP");
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            String backendUrl = pathsProperties.getPathValue("backend.url") + "/group/GroupController.php";
-            System.out.println(" 1");
-            RestTemplate restTemplate=new RestTemplate();
-            HttpHeaders requestHeaders = common.setUserCookies(pathsProperties, groupListModel.getLogin(), groupListModel.getPassword(), groupListModel.getPartnerId());
-            MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
-            map.add("userId", ""+ groupListModel.getUser_id());
-            map.add("customerId", ""+ groupListModel.getCustomer_id());
-            map.add("sharing", "0");
-            map.add("ACTION", "LIST_CONTACTS_BY_GROUP");
-            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, requestHeaders);
-            ResponseEntity<?> response = restTemplate.exchange(backendUrl, HttpMethod.POST, request, String.class);
-            //logger.trace("response 1: "+response);
-           // System.out.println("response 1: "+response);
-            //logger.trace("body : "+response.getBody());
-            System.out.println("body: "+response.getBody());
-
-            if(response==null || response.equals("") || response.getStatusCode().value()!=200) {
-                logger.trace("Error while processing your request. Please contact your administrator.");
-                logger.trace("************************** End to get contacts by group ************************************");
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error while processing your request. Please contact your administrator.");
-
-            }else {
-                ContactsGroupDto[] groups = mapper.readValue(response.getBody().toString(), ContactsGroupDto[].class);
-                //logger.trace("list contacts by group: "+ response);
-                //logger.trace("List contacts by group: "+groups);
-                logger.trace("************************** End to get contacts by group ************************************");
-                return ResponseEntity.ok(groups);
-            }
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("catch: Error while processing your request. Please contact your administrator.");
-        }
+        return ResponseEntity.ok(new ApiDtoResponse(false, "Group is successfully deleted."));
     }
 
 }
